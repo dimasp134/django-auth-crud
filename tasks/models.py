2ea9cda0eb49bcf41f173080cd7286243f2a67d8
dimas <dimasjp8@gmail.com>
2022-10-15 18:45:27 -0400

    first commit

---
+++ tasks/models.py
@@ -0,0 +1,16 @@
+from enum import auto
+from tkinter import Widget
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Tasks(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank= True)
+    datecreated = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True,blank=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.title + "-by " + self.user.username
---
+++ tasks/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+urlpatterns = [path("", views.home, name="home"),
+               path("signup/", views.signup, name="signup"),
+               path("logout/", views.signout, name="logout"),
+               path("tasks/", views.tasks, name="tasks"),
+               path("tasks_completed/", views.tasks_completed, name="tasks_completed"),
+               path("tasks/<int:task_id>/", views.task_detail, name="task_detail"),
+               path("tasks_deleted/<int:task_id>", views.tasks_remove, name="tasks_deleted"),
+               path("tasks/<int:task_id>/complete", views.task_complete, name="task_complete"),
+               path("signin/", views.signin, name="signin"),
+               path("tasks/create/",views.create_task, name ="create_task")
+
+               ]
